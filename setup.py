8f241894b04d4b4c94608ce334fb35e14c0f2c02
soham2002-btech <78204011+soham2002-btech@users.noreply.github.com>
2024-09-07 20:15:26 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'Alok kumar',
+    author_email= 'ay747283@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
